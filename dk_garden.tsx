910c47d4f5383d25a983eb5ff101aea1351a55ee
holymoly <ztl.light@hackfres.se>
2020-12-28 10:42:20 +0100

    first test

---
+++ dk_garden.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="dk_garden" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="tiles_png/structures/dk_garden.png" width="512" height="384"/>
+</tileset>
---
+++ dk_transports.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="dk_transports" tilewidth="32" tileheight="32" tilecount="48" columns="16">
+ <image source="tiles_png/inventory/dk_transports.png" width="512" height="96"/>
+</tileset>
