7c7971ccff45f3685f5351fa88a5e499cabff967
banshee091 <banshee091@gmail.com>
2020-12-28 19:18:32 +0100

    Websites angepasst, exitschild

---
+++ minideko.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="minideko" tilewidth="32" tileheight="32" tilecount="91" columns="7">
+ <image source="tiles_png/deco/minideko.png" width="224" height="416"/>
+ <tile id="25">
+  <animation>
+   <frame tileid="25" duration="450"/>
+   <frame tileid="26" duration="450"/>
+   <frame tileid="32" duration="450"/>
+   <frame tileid="33" duration="450"/>
+   <frame tileid="39" duration="450"/>
+   <frame tileid="40" duration="450"/>
+  </animation>
+ </tile>
+</tileset>
