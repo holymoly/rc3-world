534d3c363376c1e93b4a51ce3a41fdc10e0366f9
banshee091 <banshee091@gmail.com>
2020-12-28 20:36:21 +0100

    Deko

--- minideko.tsx
+++ minideko.tsx
@@ -1,6 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="minideko" tilewidth="32" tileheight="32" tilecount="91" columns="7">
  <image source="tiles_png/deco/minideko.png" width="224" height="416"/>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="800"/>
+   <frame tileid="20" duration="800"/>
+   <frame tileid="27" duration="800"/>
+  </animation>
+ </tile>
  <tile id="25">
   <animation>
    <frame tileid="25" duration="450"/>
@@ -9,6 +16,20 @@
    <frame tileid="33" duration="450"/>
    <frame tileid="39" duration="450"/>
    <frame tileid="40" duration="450"/>
+  </animation>
+ </tile>
+ <tile id="33">
+  <animation>
+   <frame tileid="33" duration="1500"/>
+   <frame tileid="40" duration="1500"/>
+   <frame tileid="25" duration="1500"/>
+   <frame tileid="39" duration="1500"/>
+   <frame tileid="32" duration="800"/>
+   <frame tileid="39" duration="800"/>
+   <frame tileid="32" duration="800"/>
+   <frame tileid="39" duration="800"/>
+   <frame tileid="32" duration="800"/>
+   <frame tileid="39" duration="800"/>
   </animation>
  </tile>
  <tile id="69">
