053d75d865d4b0c8ca8e1d1dda6f8fb0c30c08d0
banshee091 <banshee091@gmail.com>
2020-12-29 01:39:24 +0100

    Garten und Boden Aufgehübscht

--- kaktus.tsx
+++ kaktus.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="kaktus" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="kaktus.png" width="128" height="128"/>
+<tileset version="1.4" tiledversion="1.4.3" name="kaktus" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="kaktus.png" width="256" height="256"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="1500"/>
