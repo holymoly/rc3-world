ac8d90b09eaaceb97baecea0f2815e5d5b6e01dc
banshee091 <banshee091@gmail.com>
2021-06-08 19:54:18 +0200

    Revert "naechster Deloreantest"
    This reverts commit e9e1448770ad854cbe3c121188e20ec82c0fb4ee.

--- Interiors_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Interiors_32x32" tilewidth="32" tileheight="32" tilecount="13376" columns="16">
- <image source="tiles_png/ressource_pack/Interiors_32x32.png" width="512" height="26752"/>
-</tileset>
